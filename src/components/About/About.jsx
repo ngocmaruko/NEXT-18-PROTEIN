69c4128a65cccbc4e40c465c641b80a2934820f8
Ngoc <ngoc.held9@gmail.com>
2024-10-18 17:36:01 +0900

    About

---
+++ src/components/About/About.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './About.css'; // Import the CSS file for styling
+import tablet from '../../assets/tablet.jpg';
+
+const About = () => {
+  return (
+    <section className='inner'>
+      <div className="about-section">
+        <h1>NEXT-18PROTEIN</h1>
+        <h3>スクスク伸びる成長期のカラダに必要な栄養素をシッカリ配合！</h3>
+        <p>
+          運動も勉強も人一倍頑張るキッズたちは、人一倍エネルギーと栄養が必要です。<br />
+          そんなキッズたちのため「短時間で手軽においしくエネルギーと栄養が摂取できる商品」をコンセプトに開発した商品です。
+        </p>
+
+        <div className="image-grid">
+          <div className="image-item">
+            <p>WPH <br /> プロテイン</p>
+            <div className="image-circle">
+              <img src={tablet} alt="WPHプロテイン" />
+            </div>
+          </div>
+          <div className="image-item">
+            <p>MTC <br /> オイル</p>
+            <div className="image-circle">
+              <img src={tablet} alt="MTCオイル" />
+            </div>
+          </div>
+          <div className="image-item">
+            <p>11種の <br /> ビタミン</p>
+            <div className="image-circle">
+              <img src={tablet} alt="11種のビタミン" />
+            </div>
+          </div>
+          <div className="image-item">
+            <p>カルシウム <br /> マグネシウム</p>
+            <div className="image-circle">
+              <img src={tablet} alt="カルシウムマグネシウム" />
+            </div>
+          </div>
+        </div>
+      </div>         
+    </section>
+  );
+};
+
+export default About;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Splash from '../../components/Splash/Splash';
 import Hero from '../Hero/Hero';
+import About from '../../components/About/About';
 
 const HomePage = () => {
   return (
     <Splash>
       {/* Content that should be shown after the splash animation */}
     <Hero />
+    <About />
     </Splash>
   );
 };
